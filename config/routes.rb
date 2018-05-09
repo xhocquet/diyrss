b7f44a8e1f582e018594501bf0777a812ba08670
Xavier Hocquet <xhocquet@gmail.com>
2018-05-08 18:15:47 -0600

    Mount sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   root 'application#index'
   get 'about', controller: 'application', action: 'about'
   get 'redirect/:monitor_id', controller: :redirects, action: :redirect, as: :redirect
 
   devise_for :users
+
+  mount Sidekiq::Web => '/sidekiq'
 
   resources :user_categories do
     resources :user_monitors, shallow: true
