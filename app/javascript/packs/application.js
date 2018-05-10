d658343ccec2642a3f265224fea2337bb927807c
Xavier Hocquet <xhocquet@gmail.com>
2018-05-09 21:49:47 -0600

    Add CSS to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,65 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
+/* eslint no-console: 0 */
 
-console.log('Hello World from Webpacker')
+import "jquery"
+import "jquery-ujs"
+import "bootstrap"
+import "jquery-easing"
+
+import '../stylesheets/application'
+
+document.addEventListener('DOMContentLoaded', () => {
+      // Configure tooltips for collapsed side navigation
+      $('.navbar-sidenav [data-toggle="tooltip"]').tooltip({
+        template: '<div class="tooltip navbar-sidenav-tooltip" role="tooltip" style="pointer-events: none;"><div class="arrow"></div><div class="tooltip-inner"></div></div>'
+      })
+      // Toggle the side navigation
+      $("#sidenavToggler").click(function(e) {
+        e.preventDefault();
+        $("body").toggleClass("sidenav-toggled");
+        $(".navbar-sidenav .nav-link-collapse").addClass("collapsed");
+        $(".navbar-sidenav .sidenav-second-level, .navbar-sidenav .sidenav-third-level").removeClass("show");
+      });
+      // Force the toggled class to be removed when a collapsible nav link is clicked
+      $(".navbar-sidenav .nav-link-collapse").click(function(e) {
+        e.preventDefault();
+        $("body").removeClass("sidenav-toggled");
+      });
+      // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
+      $('body.fixed-nav .navbar-sidenav, body.fixed-nav .sidenav-toggler, body.fixed-nav .navbar-collapse').on('mousewheel DOMMouseScroll', function(e) {
+        var e0 = e.originalEvent,
+          delta = e0.wheelDelta || -e0.detail;
+        this.scrollTop += (delta < 0 ? 1 : -1) * 30;
+        e.preventDefault();
+      });
+      // Scroll to top button appear
+      $(document).scroll(function() {
+        var scrollDistance = $(this).scrollTop();
+        if (scrollDistance > 100) {
+          $('.scroll-to-top').fadeIn();
+        } else {
+          $('.scroll-to-top').fadeOut();
+        }
+      });
+      // Configure tooltips globally
+      $('[data-toggle="tooltip"]').tooltip()
+      // Smooth scrolling using jQuery easing
+      $(document).on('click', 'a.scroll-to-top', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top)
+        }, 1000, 'easeInOutExpo');
+        event.preventDefault();
+      });
+
+      $('.alert').each(function(_i, elem) {
+        window.setTimeout(function() {
+          $(elem).fadeOut("slow");
+        }, 3000);
+      })
+
+      $('.toggle-link-form-button').click(function(e) {
+        e.preventDefault();
+        $(e.target).parents('.link-container').find('.link-form').toggleClass('toggled');
+      });
+});
+
