eadb190091b88b2abb21ee176234517ae031e90f
Xavier Hocquet <xhocquet@gmail.com>
2018-05-20 22:52:54 -0600

    Update script to update gems, npm, and migrate

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,9 +1,14 @@
-x = `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
-x = `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
+`ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
+`ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
 
-x = `git stash`
-x = `git pull`
-x = `git stash pop`
+`git stash`
+`git pull`
 
-x = `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
-x = `RAILS_ENV=production RACK_ENV=production bundle exec sidekiq -d -L log/sidekiq.log`
+`bundle install`
+`yarn`
+`rails db:migrate`
+
+`git stash pop`
+
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
+`RAILS_ENV=production RACK_ENV=production bundle exec sidekiq -d -L log/sidekiq.log`
