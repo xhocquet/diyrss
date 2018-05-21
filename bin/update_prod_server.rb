44b9308821e78512ba9d1c0be107a45c31e386c6
Xavier Hocquet <xhocquet@gmail.com>
2018-05-20 22:55:12 -0600

    Update env vars

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,3 +1,7 @@
+`export RAILS_ENV=production`
+`export RACK_ENV=production`
+`export RAILS_SERVE_STATIC_FILES=true`
+
 `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
 `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
 
@@ -10,5 +14,5 @@
 
 `git stash pop`
 
-`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
-`RAILS_ENV=production RACK_ENV=production bundle exec sidekiq -d -L log/sidekiq.log`
+`bundle exec puma -C config/puma.rb -p 80 -d`
+`bundle exec sidekiq -d -L log/sidekiq.log`
