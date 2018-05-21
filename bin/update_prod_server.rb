55ff23c80d9846327639b7659e470e40f73993d4
Xavier Hocquet <xhocquet@gmail.com>
2018-05-20 18:55:20 -0600

    backtick notation

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,9 +1,9 @@
-exec "ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9"
-exec "ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9"
+x = `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
+x = `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
 
-exec "git stash"
-exec "git pull"
-exec "git stash pop"
+x = `git stash`
+x = `git pull`
+x = `git stash pop`
 
-exec "RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d"
-exec "RAILS_ENV=production RACK_ENV=production bundle exec sidekiq -d -L log/sidekiq.log"
+x = `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
+x = `RAILS_ENV=production RACK_ENV=production bundle exec sidekiq -d -L log/sidekiq.log`
