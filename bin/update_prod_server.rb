8d018926d571806007e4de1dbde26718650d7c3a
Xavier Hocquet <xhocquet@gmail.com>
2018-05-21 20:12:18 -0600

    Try something else

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,18 +1,14 @@
-`export RAILS_ENV=production`
-`export RACK_ENV=production`
-`export RAILS_SERVE_STATIC_FILES=true`
-
 `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
 `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
 
 `git stash`
 `git pull`
 
-`bundle install`
-`yarn`
-`rails db:migrate`
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle install`
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true yarn`
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true rails db:migrate`
 
 `git stash pop`
 
-`bundle exec puma -C config/puma.rb -p 80 -d`
-`bundle exec sidekiq -d -L log/sidekiq.log`
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
+`RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec sidekiq -d -L log/sidekiq.log`
