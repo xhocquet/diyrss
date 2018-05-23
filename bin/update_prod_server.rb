071437f7f485c5601f51e631835254299ff88d20
Xavier Hocquet <xhocquet@gmail.com>
2018-05-22 21:02:20 -0600

    Add logs to update script
    Blast
    
    I could just test these

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,14 +1,23 @@
+puts "Stopping Sidekiq and Puma..."
 `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
 `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
+puts "Sidekiq and Puma stopped"
 
+puts "Stashing production assets"
 `git stash`
 `git pull`
 
+puts "Updating gems"
 `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle install`
+puts "Updating node modules"
 `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true yarn`
+puts "Migrating database"
 `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true rails db:migrate`
 
+puts "Restoring stashed production assets"
 `git stash pop`
 
+puts "Restarting Puma and Sidekiq..."
 `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec puma -C config/puma.rb -p 80 -d`
 `RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true bundle exec sidekiq -d -L log/sidekiq.log`
+puts "Puma and Sidekiq restarted"
