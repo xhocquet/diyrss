61124bc740f7789808f59cc795e71b0a3e46ef1f
Xavier Hocquet <xhocquet@gmail.com>
2018-05-28 10:54:58 -0600

    Get rid of rollbar on dev

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -5,7 +5,7 @@ Rollbar.configure do |config|
   config.access_token = Rails.application.secrets.try(:rollbar_key)
 
   # Here we'll disable in 'test':
-  if Rails.env.test?
+  if !Rails.env.production?
     config.enabled = false
   end
 
