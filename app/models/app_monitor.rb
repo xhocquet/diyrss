54e18f685cf555b8d9ce23c1a060b8aa44ea6fd8
Xavier Hocquet <xhocquet@gmail.com>
2018-06-03 19:03:46 -0600

    Schedule favicon job on new app monitor

--- app/models/app_monitor.rb
+++ app/models/app_monitor.rb
@@ -1,5 +1,5 @@
 class AppMonitor < ApplicationRecord
-  after_commit :trigger_initial_job, on: :create
+  after_commit :trigger_initial_jobs, on: :create
 
   has_many :monitor_results, inverse_of: :app_monitor, dependent: :destroy
   has_many :user_monitors, inverse_of: :app_monitor, dependent: :destroy
@@ -20,7 +20,8 @@ class AppMonitor < ApplicationRecord
   end
 
   private
-    def trigger_initial_job
+    def trigger_initial_jobs
+      FetchFaviconWorker.perform_async(id)
       AppMonitorWorker.perform_async(id)
     end
 end
