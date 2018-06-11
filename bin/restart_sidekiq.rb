e496365e614ca65714788b60dfc4fdfe71eb3aae
Xavier Hocquet <xhocquet@gmail.com>
2018-06-10 21:37:08 -0600

    Clean up configs for easy monitoring

--- bin/restart_sidekiq.rb
+++
@@ -1,7 +0,0 @@
-ENV_VARS = "RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true"
-
-puts "Stopping Sidekiq and Puma..."
-`ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
-
-puts "Restarting Puma and Sidekiq..."
-`#{ENV_VARS} bundle exec sidekiq -q default -q mailers -d -L log/sidekiq.log`
--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -22,6 +22,6 @@ puts "Updating CRON tab"
 `#{ENV_VARS} whenever --update-crontab`
 
 puts "Restarting Puma and Sidekiq..."
-`#{ENV_VARS} bundle exec puma -C config/puma.rb -p 80 -d`
-`#{ENV_VARS} bundle exec sidekiq -q default -q mailers -d -L log/sidekiq.log`
+`#{ENV_VARS} bundle exec puma -C config/puma.rb`
+`#{ENV_VARS} bundle exec sidekiq -d -C config/sidekiq.yml`
 puts "Puma and Sidekiq restarted"
