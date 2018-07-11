397eba991600c94624b02b217eb469b5900dc813
Xavier Hocquet <xhocquet@gmail.com>
2018-07-10 23:14:46 -0600

    Try again!

--- bin/update_prod_server.rb
+++ bin/update_prod_server.rb
@@ -1,17 +1,18 @@
 ENV_VARS = "RAILS_ENV=production RACK_ENV=production RAILS_SERVE_STATIC_FILES=true"
+BUNDLE = "/usr/share/rvm/gems/ruby-2.4.1/bin/bundle"
 
 `ps -ef | grep sidekiq | grep -v grep | awk '{print $2}' | xargs kill -9`
 `ps -ef | grep puma | grep -v grep | awk '{print $2}' | xargs kill -9`
 
-`bundle exec rails assets:clobber`
+`#{BUNDLE} exec rails assets:clobber`
 `git pull`
 
-`#{ENV_VARS} bundle install`
+`#{ENV_VARS} #{BUNDLE} install`
 `#{ENV_VARS} npm install`
-`#{ENV_VARS} bundle exec rails assets:precompile`
-`#{ENV_VARS} bundle exec rails db:migrate`
+`#{ENV_VARS} #{BUNDLE} exec rails assets:precompile`
+`#{ENV_VARS} #{BUNDLE} exec rails db:migrate`
 
 `#{ENV_VARS} whenever --update-crontab`
 
-`#{ENV_VARS} bundle exec puma -C config/puma.rb`
-`#{ENV_VARS} bundle exec sidekiq -d -C config/sidekiq.yml`
+`#{ENV_VARS} #{BUNDLE} exec puma -C config/puma.rb`
+`#{ENV_VARS} #{BUNDLE} exec sidekiq -d -C config/sidekiq.yml`
