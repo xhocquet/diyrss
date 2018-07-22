982971705535d5d54e239d99f60753ff7b7ae1a9
Xavier Hocquet <xhocquet@gmail.com>
2018-07-21 19:52:33 -0600

    Update puma config

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,7 @@
 rails_env = ENV['RAILS_ENV'] || 'development'
 rails_root = File.expand_path("../", File.dirname(__FILE__))
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 
 case rails_env
   when 'production'
@@ -10,11 +12,28 @@ case rails_env
   else
 end
 
-threads 1,4
-preload_app!
+# Number of cores
+workers 2
+
+# Min, max threads
+threads 1,6
+
+bind "unix://#{shared_dir}/sockets/puma.sock"
+
+# preload_app!
+
+# Logging
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
 environment ENV.fetch("RAILS_ENV") { "development" }
 
-pidfile "tmp/pids/puma.pid"
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
 
-plugin :tmp_restart
+on_worker_boot do
+  require "active_record"
+  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+end
+
